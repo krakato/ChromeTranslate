bfd74c7bb6883ddb5f1057411f1a481b66d723a0
krakato <7564gonzalez@gmail.com>
2025-07-18 17:48:40 +0200

    manifest.json: Update version

--- background.js
+++ background.js
@@ -7,12 +7,5 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "translate") {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        chrome.runtime.sendMessage({ action: "translate" });
-      }
-    });
-  }
+  chrome.tabs.sendMessage(tab.id, { action: "translate" });
 });
